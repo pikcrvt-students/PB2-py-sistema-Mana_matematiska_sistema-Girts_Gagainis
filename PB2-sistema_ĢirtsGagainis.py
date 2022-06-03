102afdacda2ec16282100f1af1a1e058c57ef3e4
21DP2GGaga <100911612+21DP2GGaga@users.noreply.github.com>
2022-06-03 09:00:44 +0300

    Update PB2-sistema_ĢirtsGagainis.py

--- PB2-sistema_ĢirtsGagainis.py
+++ PB2-sistema_ĢirtsGagainis.py
@@ -1,2 +1,10 @@
 figura = input("Ieraksti kādu figūru tu vēlies aprēķināt.\nizvēles ir: rinķis, trijstūris, taisnleņķa trijsturis,\nkvadrāts, taisnstūris, rombs, trapece:")
 P_vai_S = input("Ko tu vēlies šai figūrai aprēķināt? Ieraksti \"perimetrs\" vai arī \"laukums\"")
+
+if figura == "rinķis" or "Rinķis":
+    if P_vai_S == "perimetrs":
+        radiuss = float(input("Ievadi rinķa rādiusu (pi = 3.14): "))
+        print("Rinķa perimetrs ir =", 2*3.14*radiuss)
+    else:
+        radiuss = float(input("Ievadi rinķa rādiusu (pi = 3.14): "))
+        print("Rinķa laukums ir =",3.14 * radiuss^2)
