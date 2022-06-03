33e3cafd039da42aa779fea3a46a183f377361ba
21DP2GGaga <100911612+21DP2GGaga@users.noreply.github.com>
2022-06-03 08:49:41 +0300

    Update PB2-sistema_ĢirtsGagainis.py

--- PB2-sistema_ĢirtsGagainis.py
+++ PB2-sistema_ĢirtsGagainis.py
@@ -1,1 +1,2 @@
-es nezinu kas šeit ir pagaidām jāraksta, to es noskaidrošu nākamajā mācību stundā
+figura = input("Ieraksti kādu figūru tu vēlies aprēķināt.\nizvēles ir: rinķis, trijstūris, taisnleņķa trijsturis,\nkvadrāts, taisnstūris, rombs, trapece:")
+P_vai_S = input("Ko tu vēlies šai figūrai aprēķināt? Ieraksti \"perimetrs\" vai arī \"laukums\"")
