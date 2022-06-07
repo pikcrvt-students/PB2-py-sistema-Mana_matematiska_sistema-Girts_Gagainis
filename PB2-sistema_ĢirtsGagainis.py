a24970e3a9b84de5e769f8d1028d52aee0c4c88d
21DP2GGaga <100911612+21DP2GGaga@users.noreply.github.com>
2022-06-07 12:53:41 +0300

    Update PB2-sistema_ĢirtsGagainis.py

--- PB2-sistema_ĢirtsGagainis.py
+++ PB2-sistema_ĢirtsGagainis.py
@@ -1,16 +1,19 @@
+from random import randint
+print("Izvēlies kādu no pieejamajām tēmām: kvadrātvienādojums(), pitagora_teorēma()")
 def kvadrātvienādojums():
     gala_punkti = 0
     print("Teorija: Par kvadrātvienādojumu sauc vienādojumu, kurš ir dots (vai to var pārveidot) formā ax2+bx+c=0, kur a, b, c ir reāli skaitļi, turklāt a≠0, bet x - mainīgais.")
     print("Vispārīgā kvadrātvienādojuma ax^2+bx+c=0 saknes aprēķina, izmantojot formulu: D=b^2−4ac, un saknes ar x1 = (-b + sqrt(D))/2*a) un x2 = (-b - sqrt(D))/2*a)\n")
 
+    #1. uzd
     atbilde1 = int(input("1. uzdevums: Kāds ir diskriminants vienādojumam X^2 + 12X + 7 = 0?: "))
     if atbilde1 == 116:
         print("Pareiza atbilde! +1 punkts!\n")
         gala_punkti += 1
     else: 
         print("Nepareizi!\n")
-        
-    print("Kādas ir šī piemēra saknes? 5X^2 - 16X + 3 = 0")
+    #2. uzd
+    print("2. Uzdevums: Kādas ir šī piemēra saknes? 5X^2 - 16X + 3 = 0")
     atbilde2 = float(input("1. sakne ir: "))
     atbilde3 = float(input("2. sakne ir: "))
     if atbilde2 == 3 or atbilde2 == 0.2 and atbilde3 == 0.2 or atbilde3 == 3:
@@ -20,8 +23,24 @@ def kvadrātvienādojums():
         print("Nepareizi!\n")
     
 
-
-
+    print("Teorija: Var arī pielietot Vjeta teorēmu, lai noteiktu saknes.\n Reducēta kvadrātvienādojuma sakņu reizinājums ir vienāds ar brīvo locekli, bet sakņu summa ir vienāda ar lineārā locekļa koeficientam pretējo skaitli.")
+    print("x1 * x2 = c un x1 + x2 = -b\n")
     
+    
+    #3 uzd
+    print("3. Uzdevums: Kādas ir šī piemēra saknes? X^2 - 14X + 40 = 0")
+    atbilde4 = float(input("1. sakne ir: "))
+    atbilde5 = float(input("2. sakne ir: "))
+    if atbilde4 == 10 or atbilde4 == 4 and atbilde5 == 4 or atbilde5 == 10:
+        print("Pareiza atbilde! +1 punkts!\n")
+        gala_punkti += 1
+    else: 
+        print("Nepareizi!\n")
+
+
+
+
+
+    print("Tev ir",gala_punkti, "punkti!")
 kvadrātvienādojums()
 
