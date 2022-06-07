b6f41c28bfd6bc26ba90112de3b124b529a60e2b
21DP2GGaga <100911612+21DP2GGaga@users.noreply.github.com>
2022-06-07 12:19:21 +0300

    Update PB2-sistema_ĢirtsGagainis.py

--- PB2-sistema_ĢirtsGagainis.py
+++ PB2-sistema_ĢirtsGagainis.py
@@ -1,8 +1,20 @@
 from asyncore import read
 
-path = "C:\\Users\\girts\\Desktop\\Programmēšanas matemātikas darbs\\vardi_un_rezultati.txt"
-fails = open(path, "r+", encoding="utf-8")
+gala_punkti = 0
 
-vārds = input("Ievadi savu vārdu: ")
 
-print(fails.read())
+
+def kvadrātvienādojums():
+    print("Teorija: Par kvadrātvienādojumu sauc vienādojumu, kurš ir dots (vai to var pārveidot) formā ax2+bx+c=0, kur a, b, c ir reāli skaitļi, turklāt a≠0, bet x - mainīgais.")
+    print("Vispārīgā kvadrātvienādojuma ax2+bx+c=0 saknes aprēķina, izmantojot formulu: D=b^2−4ac, un saknes ar x = (-b + sqrt(D))/2*a")
+
+    atbilde1 = int(input("1. uzdevums: Kāds ir diskriminants vienādojumam X^2 + 5X + 12?: "))
+    if atbilde1 == :
+        print("Pareiza atbilde! +1 punkts!")
+        gala_punkti += 1
+
+
+
+
+
+
